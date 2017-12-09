6646b98886140ea5f070a9e7773db5178c5266ad
ragabala <bragavendran@yahoo.com>
2017-12-09 16:39:56 -0500

    project with missiles

--- rasterize.js
+++ rasterize.js
@@ -1,9 +1,10 @@
 var scene,camera,renderer,mesh,floor; // the all powerful objects that are needed to render objects and set view in three js
 var keyboard = {};
-var player ={ height: 3.8}
+var player ={ height: 3.8 , depth : -6}
 var textureLoader;
 var textures = {};
 var meshes = {};
+meshes.missile = [];
 
 
 function init(){
@@ -14,15 +15,73 @@ function init(){
 	renderer.setClearColor( 0x000000, 0 );
 	renderer.setSize(window.innerWidth,window.innerHeight);
 	$('body').append(renderer.domElement)
+	console.log(renderer)
 	addBackground();
 	addBuildings();
 	addMissileBlaster();
+	addMissiles();
 	addFloor();
 	setCamera();
 
 	animate();
+	console.log(camera);
+	console.log(renderer.domElement)
+
 }
 
+function deg2Rad(value){
+	return value * Math.PI / 180
+}
+
+function getRandomMissileTraversal(){
+
+ // since we will be shooting only at z = 0 
+
+//starting location of missile
+
+var missileTraversal = {};
+var start = [];
+var random = Math.random() * camera.X_LIMIT * 2 ;
+start[0] = Math.ceil(camera.X_LIMIT - random )
+start[1] = Math.ceil(camera.Y_LIMIT)
+start[2] = 0 // since we will be having Z coordinate set
+missileTraversal.start = start;
+
+var end = [];
+
+var random = Math.random() * (camera.X_LIMIT -2) * 2 ; // ensuring the bombs are within resonable X limit for destination
+end[0] = Math.ceil((camera.X_LIMIT-2) - random )
+end[1] = 0 // so that the bombs reach the ground
+end[2] = 0 // so that it is in middle of Z
+missileTraversal.end = end;
+console.log("traversal " ,missileTraversal)
+return missileTraversal;
+}
+
+
+
+
+function addMissiles(){
+
+	var missileTraversal = getRandomMissileTraversal();
+	var positions = [13,7,0]; // x and y positions
+	var counter = 0;
+	textureLoader = new THREE.TextureLoader()
+	textures.missileTexture = new textureLoader.load("textures/missile/png/11.png")
+	
+	meshes.missile[meshes.missile.length] = new THREE.Mesh(
+		new THREE.SphereGeometry(0.25),
+		new THREE.MeshBasicMaterial({ color: 0xf442d1, wireframe: false})
+	)
+	console.log(meshes.missile)
+	currentMissile = meshes.missile[meshes.missile.length -1 ]
+	
+	currentMissile.position.x = positions[0];
+	currentMissile.position.y = positions[1];
+	currentMissile.position.z = positions[2];
+	scene.add(currentMissile);
+
+}
 
 function addBackground(){
 	//adding a box
@@ -74,7 +133,7 @@ function addMissileBlaster(){
 	meshes.missileBlaster.material.side = THREE.DoubleSide;
 	meshes.missileBlaster.position.x = position[0]
 	meshes.missileBlaster.position.z = position[1]
-	meshes.missileBlaster.position.y = 1
+	meshes.missileBlaster.position.y = 1.3
 	scene.add(meshes.missileBlaster)
 
 
@@ -127,14 +186,22 @@ function addFloor(){
 
 function setCamera(){
 // setting the camera away from the origin and at the height of the player
-	camera.position.set(0,player.height,-5); // since the objects by default appears at 0,0,0
-	camera.lookAt(new THREE.Vector3(0,player.height,0))
+// these values are useful in setting missile positions to FOV
+	 console.log("fov",camera.fov)
+	 camera.X_LIMIT = Math.tan(deg2Rad(camera.fov / 2 )) * 2;
+	 renderer.aspect_r  = renderer.domElement.width
+	 camera.Y_LIMIT = camera.X_LIMIT / renderer.aspect_r;
+	 camera.Z_LIMIT = 0
+
+	camera.position.set(0,player.height, player.depth); // since the objects by default appears at 0,0,0
+	camera.lookAt(new THREE.Vector3(0,3.8,0))
+
 
 
 }
 
 function animate(){
-	mesh = meshes.missileBlaster
+	mesh = meshes.missile[meshes.missile.length -1 ]
 	requestAnimationFrame(animate)
 	if(keyboard[37])
 	mesh.rotation.y += 0.01
@@ -144,6 +211,17 @@ function animate(){
 	mesh.rotation.x += 0.01
 	if(keyboard[40])
 	mesh.rotation.x -= 0.01
+
+	if(keyboard[87])
+	mesh.position.y += 0.01
+	if(keyboard[83])
+		mesh.position.y -= 0.01
+	if(keyboard[65])
+		mesh.position.x -= 0.01
+	if(keyboard[68])
+		mesh.position.x += 0.01
+
+
 
 	renderer.render(scene,camera)
 
